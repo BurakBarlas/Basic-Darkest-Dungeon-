3561dec97491d19fa0245d66c13bdb7228e13c1b
BurakBarlas <55807664+BurakBarlas@users.noreply.github.com>
2020-04-27 11:36:38 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 class Unit {
+protected:
+    int maxHp;
+    int dodge;
+    int prot;
+    int speed;
+    int accMod;
+    int baseCrit;
+    int minDmg;
+    int maxDmg;
+    int stunResist;
 
 };
 class Hero: public Unit{
-
+    int deathBlowResist;
 };
 class Crusader: public Hero {
     void attack(Unit attackedUnit);
@@ -29,10 +41,17 @@ class StunSkill{
 
 };
 class Skill{
+public:
+    int position;
+    int target;
 
 };
 class AttackSkill: public Skill{
-
+    int dmgMod;
+    int baseAcc;
+    int critMod;
+    string stunEffect;
+    int stunBase;
 };
 class Smite: public AttackSkill{
 
@@ -41,7 +60,9 @@ class Stunning_Blow: public AttackSkill, public StunSkill{
 
 };
 class UtilitySkill: public Skill{
-
+    int position;
+    string Target;
+    // string effect gelecek +20 Prot for 3 round
 };
 class Divine_Grace: public UtilitySkill{
 
@@ -52,12 +73,17 @@ class MoveSkill: public Skill{
 
 
 
+class attack: public Skill,public Unit{
+
+};
 
 
+void turnOrder(){
+
+}
 
 
 int main() {
-
 
     return 0;
 }
